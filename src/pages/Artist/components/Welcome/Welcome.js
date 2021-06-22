267c0a8970356799d3e02dc8652dce465644743a
Mason Vale <mason.vale@square63.org>
2021-06-22 11:08:35 +0500

    Implemented welcome metadata content api

--- src/pages/Artist/components/Welcome/Welcome.js
+++ src/pages/Artist/components/Welcome/Welcome.js
@@ -1,26 +1,51 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./Welcome.scss";
-import {Link, NavLink} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import ArrowRight from "../../../../images/right-arrow.svg";
+import {ACCESS_TOKEN, WELCOME_METADATA, BASE_URL} from "../../../../common/api";
+import Notiflix from "notiflix-react";
+import Loader from "../../../../images/loader.svg";
 
 function Welcome() {
   const role = JSON.parse(localStorage.getItem("userRole") ?? "");
+  const [metaData, setMetadata] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    fetchWelcomeMetadata();
+  }, [])
+
+  const fetchWelcomeMetadata = async () => {
+    setIsLoading(true);
+    const userAuthToken = JSON.parse(localStorage.getItem("user") ?? "");
+    const response = await fetch(`${BASE_URL}${WELCOME_METADATA}`,
+      {
+        headers: {
+          "authorization": ACCESS_TOKEN,
+          "auth-token": userAuthToken
+        }
+      });
+    if (!response.ok) {
+      setMetadata(null);
+      Notiflix.Notify.Failure("Can't connect to server, try later!");
+    } else {
+      const resultSet = await response.json();
+      if(resultSet['content']) {
+        setMetadata(resultSet['content']);
+      }
+    }
+    setIsLoading(false);
+  }
+
   return (
     <div className="welcome-content">
-      <div class="next-btn">
+      <div className="next-btn">
         <NavLink to={role === "artist" ? "/agreements" : "/invites"} className="btn primary-btn next">Next <img className="" src={ArrowRight} alt="proceed-icon"/></NavLink>
       </div>
-      <section>
-        <h3><strong>What happens now?</strong></h3>
-        <h5><strong>Good question! Try these on for size:</strong></h5>
-        <ul>
-          <li>Read the Getting paid section below and send us your info via mail or scan.</li>
-          <li>Tell us more about you. We need your phone number and address so we can keep in touch! Click <Link to="/profile">Artist</Link> to the left.</li>
-          <li>Add collaborators for this artist. Collaborators are your writers, record label reps, managers and publishers. You have to invite all the writers and they must accept contract terms before we can accept your music in our catalogue. To do so, click on <Link to="/partners">Partners</Link> to the left.</li>
-          <li>Upload some music! Our crack team of musicologists will carefully evaluate it as soon as they can. Click <Link to="/music">Music</Link> to the left.</li>
-        </ul>
-      </section>
-
+      {isLoading && <h5>Loading... <img className="loading" src={Loader} alt="loading-icon"/></h5>}
+      {metaData &&
+        <section dangerouslySetInnerHTML={{__html: metaData}}/>
+      }
       <section>
         <h3><strong>Question?</strong></h3>
         <h5><strong>If you have any questions or suggestions, please send us an email at <a href="mailto:artists@audiosocket.com">artists@audiosocket.com</a>.</strong></h5>
