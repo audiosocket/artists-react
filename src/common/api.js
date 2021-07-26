4774b8f8ec0abfb4cb8b7842a8a79208e1b8365e
Mason Vale <mason.vale@square63.org>
2021-07-26 10:08:49 +0500

    Updated base url of backend server

--- src/common/api.js
+++ src/common/api.js
@@ -1,5 +1,5 @@
 export const ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhcHBfaWQiOiJhcnRpc3RzLXBvcnRhbC1iYWNrZW5kIn0.etBLEBaghaQBvyYoz1Veu6hvJBZpyL668dfkrRNLla8";
-export const BASE_URL = "http://artist_portal_backend.square63.com/";
+export const BASE_URL = "http://artist-portal-backend.square63.net/";
 export const SESSION = "api/v1/session";
 export const AUTHENTICATE_TOKEN = "api/v1/users/authenticate_token";
 export const AUTHENTICATE_COLLABORATOR_TOKEN = "api/v1/artists_collaborators/authenticate_token";
