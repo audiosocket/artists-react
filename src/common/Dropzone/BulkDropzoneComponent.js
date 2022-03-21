3cc5e732a064b4fa19ad9cd98e2a6094e9e7cb14
usman sajid <usman.sajid@square63.org>
2022-03-21 12:20:29 +0500

    added limit and added helping text for bulk upload

--- src/common/Dropzone/BulkDropzoneComponent.js
+++ src/common/Dropzone/BulkDropzoneComponent.js
@@ -127,7 +127,8 @@ const DropzoneComponent = ({onUploadFiles = null}) => {
             onChange={filesSelected}
           />
         </div>
-        <small>Please submit music files (WAV or AIFF) at 16bit or 24bit, at 44K, 44.1K or 48K.</small>
+        <small>Please submit music files (WAV or AIFF) at 16bit or 24bit, at 44K, 44.1K or 48K.</small><br></br>
+        <small>You can upload maximum 20 tracks at a time.</small>
         <div className="file-display-container">
           {
             validFiles.map((data, i) =>
